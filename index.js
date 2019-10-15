c5cef009e71f8e36c8588408c9c1dc304d51936e
Alexandria <alex.pugia816@hotmail.com>
2019-10-15 17:49:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
+  let username;
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    username = document.getElementById('name-input').value
+
+    if (username === "") {
+      alert("Name cannot be blank!")
+    } else {
+      fetchJoke()
+    }
+  })
 
   function fetchJoke(){
     fetch('https://icanhazdadjoke.com/', {
@@ -12,16 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(data => {
+      joke = data.joke
+
+      //appends new joke to list
+      const newJokeLi = document.createElement('li')
+      newJokeLi.innerHTML = `<span class="username">${username} says:</span> ${joke}`
+      jokeList.appendChild(newJokeLi)
+    })
   }
-
-  form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
-  })
 })
